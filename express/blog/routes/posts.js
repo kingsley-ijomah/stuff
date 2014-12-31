c367c8ae995c8591738ade499a475e68cef17701
kingsley ijomah <kingsley.ijomah@gmail.com>
2014-12-31 15:32:03 +0000

    Install required modules and add posts route

---
+++ express/blog/routes/posts.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+
+router.route('/posts')
+   // create a post (accessed at POST http://localhost:3000/posts)
+  .post(function(req, res) {
+
+  })
+
+  // get all the posts (accessed at GET http://localhost:3000/posts)
+  .get(function(req, res) {
+
+  });
+
+router.route('/posts/:post_id')
+  // get the post with that id (accessed at GET http://localhost:3000/posts/:post_id)
+  .get(function(req, res) {
+
+  })
+
+  // update the post with this id (accessed at PUT http://localhost:3000/posts/:post_id)
+  .put(function(req, res) {
+
+   })
+
+  // delete the post with this id (accessed at DELETE http://localhost:3000/posts/:post_id)
+  .delete(function(req, res) {
+
+  });
+
+  module.exports = router;
