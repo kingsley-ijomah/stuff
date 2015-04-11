87d787bb98be1f227580e42cdbdc25c359c107d9
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-04-11 23:23:45 +0100

    Remove todos

--- todos/db/migrate/20150409132213_create_items.rb
+++
@@ -1,7 +0,0 @@
-class CreateItems < ActiveRecord::Migration
-  def change
-    create_table :items do |t|
-      t.string :name
-    end
-  end
-end
--- todos/spec/models/item_model_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-describe Item do
-	it 'requires name field to be valid' do
-		item = Item.create({name: nil})
-		expect(item).to_not be_valid
-		expect(item.errors[:name].size).to eq(1)
-	end
-end
