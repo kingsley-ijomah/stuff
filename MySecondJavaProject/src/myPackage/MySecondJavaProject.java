0f05c121bd174e65e1d6336d37fa8fe2f47134a0
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-05-20 15:29:06 +0100

    Dummy project

---
+++ MySecondJavaProject/src/myPackage/MySecondJavaProject.java
@@ -0,0 +1,14 @@
+package myPackage;
+
+/**
+ * This is a sample java application, it prints a short
+ * statement to the standard output
+ * 
+ * @author Kingsley
+ *
+ */
+public class MySecondJavaProject {
+	public static void main(String[] args) {
+		System.out.println("This is test output!");
+	}
+}
