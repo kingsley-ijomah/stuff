d0bf3eff5c1ddf762298df1582cd579f87ec236f
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-05-25 20:09:30 +0100

    Add ability to read from file

--- InputOutput/src/io/InputOutput.java
+++ InputOutput/src/io/InputOutput.java
@@ -1,5 +1,22 @@
 package io;
 
-public class InputOutput {
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
+public class InputOutput {
+	public static void main(String[] args) {
+		List<String> groceries = new ArrayList<>();
+		try {
+			groceries = Files.readAllLines(Paths.get("/tmp/groceries.txt"), Charset.defaultCharset());
+		} catch(IOException | SecurityException e) {
+			e.printStackTrace();
+		}
+		for (String item : groceries) {
+			System.out.println("Dont forget to pick up: " + item);
+		}
+	}
 }
